d2f05eb603359fb1f9ce48e5f99ea2fe153cf76b
speedie <speedie@duck.com>
2023-03-11 21:06:37 +0100

    add license details to the top

--- src/packr.c
+++ src/packr.c
@@ -1,5 +1,19 @@
 /* packr - package manager for GNU/Linux
  *
+ * Copyright (C) 2023  speedie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * if you don't know where to start with reading the source code, start by read the main function.
  */
 
@@ -8,8 +22,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "main.c"
 #include "main.h"
+#include "main.c"
 
 #ifndef VERSION
 #define VERSION "unknown"
